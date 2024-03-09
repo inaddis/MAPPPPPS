03512dceadec62a2201e747cfcf217cf078fc2f1
inaddis <145146147+inaddis@users.noreply.github.com>
2024-03-08 18:42:16 -0700

    Create Ship.cpp

---
+++ Ship.cpp
@@ -0,0 +1,52 @@
+#include "Ship.hpp"
+
+Ship::Ship(const std::string& shipName) : shipName(shipName), numCows(0) {}
+
+    Ship::~Ship() {
+      std::cout << shipName << " was destroyed..." << std::endl;
+      for (auto& pair : cowInventory) {
+        delete pair.second;
+      }
+    }
+
+    void Ship::abductCow(Cow* cow) {
+      auto result = cowInventory.insert({ cow->getName(), cow });
+      if (result.second) {
+        numCows++;
+        std::cout << cow->getName() << " was abducted :O" << std::endl;
+      }
+      else {
+        std::cout << "Error: There's already a cow with that name >:( abduction failed" << std::endl;
+        delete cow;
+      }
+    }
+
+  const Ship::std::map<std::string, Cow*>& getCowInventory() const {
+    return cowInventory;
+  }
+
+  void Ship::dropOffCow() {
+    if (cowInventory.empty()) { 
+      std::cout << "we don't have any cows :(" << std::endl;
+      return;
+    }
+
+    auto it = std::next(cowInventory.begin(), cowInventory.size () / 2);
+    delete it->second;
+    cowInventory.erase(it);
+    numCows--;
+    
+    std::cout << "we lost a cow :(" << std::endl;
+  }
+  void Ship::dropOffCow(const std::string& cowID) {
+    auto it = cowInventory.find(cowID);
+    if (it != cowInventory.end()) {
+      delete it->second;
+      cowInventory.erase(it);
+      numCows--;
+      std::cout << cowID << " was dropped off." << std::endl;
+    }
+    else {
+      std::cout << "Error: " << cowID << " doesn't exist :|" << std::endl;
+    }
+  }
